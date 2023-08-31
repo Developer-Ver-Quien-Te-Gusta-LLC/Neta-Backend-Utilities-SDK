fa73e117cac8c5f4281de0045a89bc77a6a2e9fb
Daxx <64298070+Daxxboii@users.noreply.github.com>
2023-09-01 00:09:27 +0530

    Update Recommendations.js

--- utils/Recommendations.js
+++ utils/Recommendations.js
@@ -12,24 +12,11 @@ cassandra
   .then((CassandraClient) => (client = CassandraClient));
 
 let SameGradeWeightOnboarding,
-  SameGradeWeightExplore,
-  SameGradeWeightQuestions,
   SameHighSchoolWeightOnboarding,
-  SameHighSchoolWeightExplore,
-  SameHighSchoolWeightQuestions,
   FriendsWeightOnboarding,
-  FriendsWeightExplore,
-  FriendsWeightQuestions,
   FriendsOfFriendsWeightOnboarding,
-  FriendsOfFriendsWeightExplore,
-  FriendsOfFriendsWeightQuestions,
   EmojiContactsWeightOnboarding,
-  EmojiContactsWeightExplore,
-  EmojiContactsWeightQuestions,
-  ContactsWeightOnboarding,
-  ContactsWeightExplore,
-  ContactsWeightQuestions,
-  TopFriendsWeightsQuestions;
+  ContactsWeightOnboarding;
 
 async function fetchWeights() {
   // Fetch all weights concurrently
@@ -40,68 +27,26 @@ async function fetchWeights() {
     FriendsOfFriendsWeights,
     EmojiContactsWeights,
     ContactsWeights,
-    TopFriendsWeights,
   ] = await Promise.allSettled([
-    getKV([
-      "SameGradeWeightOnboarding",
-      "SameGradeWeightExplore",
-      "SameGradeWeightQuestions",
-    ]),
-    getKV([
-      "SameHighSchoolWeightOnboarding",
-      "SameHighSchoolWeightExplore",
-      "SameHighSchoolWeightQuestions",
-    ]),
-    getKV([
-      "FriendsWeightOnboarding",
-      "FriendsWeightExplore",
-      "FriendsWeightQuestions",
-    ]),
-    getKV([
-      "FriendsOfFriendsWeightOnboarding",
-      "FriendsOfFriendsWeightExplore",
-      "FriendsOfFriendsWeightQuestions",
-    ]),
-    getKV([
-      "EmojiContactsWeightOnboarding",
-      "EmojiContactsWeightExplore",
-      "EmojiContactsWeightQuestions",
-    ]),
-    getKV([
-      "ContactsWeightOnboarding",
-      "ContactsWeightExplore",
-      "ContactsWeightQuestions",
-    ]),
-    getKV(["TopFriendsWeightsQuestions"]),
+    getKV(["SameGradeWeightOnboarding"]),
+    getKV(["SameHighSchoolWeightOnboarding"]),
+    getKV(["FriendsWeightOnboarding"]),
+    getKV(["FriendsOfFriendsWeightOnboarding"]),
+    getKV(["EmojiContactsWeightOnboarding"]),
+    getKV(["ContactsWeightOnboarding"]),
   ]);
+
+  // Assign weights to the initialized vars
+  SameGradeWeightOnboarding = SameGradeWeights.value;
+  SameHighSchoolWeightOnboarding = SameHighSchoolWeights.value;
+  FriendsWeightOnboarding = FriendsWeights.value;
+  FriendsOfFriendsWeightOnboarding = FriendsOfFriendsWeights.value;
+  EmojiContactsWeightOnboarding = EmojiContactsWeights.value;
+  ContactsWeightOnboarding = ContactsWeights.value;
 }
 fetchWeights(); // fetch the weights as soon as the module is imported
 //#endregion
 //#region Helper Functions
-//Given a list of arrays and their weights , returns a weighted array
-//if return four is true , returns only 4 users
-function WeightArraysUsingProbability(data, weights, returnfour) {
-  // Compute the sum of all weights
-  var weightSum = weights.reduce((acc, cur) => acc + cur, 0);
-
-  // Normalize weights so that they sum to 1
-  var normalizedWeights = weights.map((weight) => weight / weightSum);
-
-  var FinalArray = [];
-  for (var i = 0; i < data.length; i++) {
-    let random = data[i]
-      .sort(() => 0.5 - Math.random())
-      .slice(0, Math.ceil(data[i].length * normalizedWeights[i]));
-    for (let i = 0; i < random.length; i++) {
-      FinalArray.push(random[i]);
-    }
-  }
-  let _FinalArray = [...new Set(FinalArray)];
-  if (returnfour) {
-    _FinalArray = _FinalArray.slice(0, 4);
-  }
-  return _FinalArray;
-}
 
 // CheckPlayerValidity function
 async function CheckPlayerValidity(username) {
@@ -156,27 +101,11 @@ async function FetchFriendsWithSubsActive(username) {
   }
 }
 
-function GetRandomFromContacts(contactsList, FavcontactsList) {
-  // Merge the two arrays
-  const mergedArray = [...contactsList, ...FavcontactsList];
-
-  // Check if the merged array is empty
-  if (mergedArray.length === 0) {
-    return null; // or you could throw an error, return a message, etc.
-  }
-
-  // Generate a random index
-  const randomIndex = Math.floor(Math.random() * mergedArray.length);
-
-  // Return a random element from the merged array
-  return mergedArray[randomIndex];
-}
-
-async function getMutualFriends(username, otherUsername) {
+async function getMutualFriends(uid, otheruid) {
   // Find friends of the main user
   const userFriends = await g
     .V()
-    .has("username", username)
+    .has("uid", uid)
     .out("FRIENDS_WITH")
     .values("username")
     .toList();
@@ -184,7 +113,7 @@ async function getMutualFriends(username, otherUsername) {
   // Find friends of the other user
   const otherUserFriends = await g
     .V()
-    .has("username", otherUsername)
+    .has("uid", otheruid)
     .out("FRIENDS_WITH")
     .values("username")
     .toList();
@@ -197,10 +126,10 @@ async function getMutualFriends(username, otherUsername) {
   return mutualFriends.length;
 }
 
-async function InsertMutualCount(username, filteredList) {
+async function InsertMutualCount(uid, filteredList) {
   index = 0;
   filteredList.forEach(async (element) => {
-    const mutualCount = await getMutualFriends(username, element.username);
+    const mutualCount = await getMutualFriends(uid, element.uid);
     filteredList[index].mutualCount = mutualCount;
     index++;
   });
@@ -210,8 +139,6 @@ async function InsertMutualCount(username, filteredList) {
 //#region Actual Fetching
 async function GetRecommendationsOnboarding(
   uid,
-  contactsList,
-  FavcontactsList,
   page_peopleYouMayKnow,
   pagesize_PeopleYouMayKnow,
   page_peopleInContacts,
@@ -224,14 +151,6 @@ async function GetRecommendationsOnboarding(
     (page_peopleYouMayKnow - 1) * pagesize_PeopleYouMayKnow;
   const offset_PeopleInContacts =
     (page_peopleInContacts - 1) * pagesize_peopleInContacts;
-
-  const userFriendsPromise = g
-    .V()
-    .has("uid", uid)
-    .out("FRIENDS_WITH")
-    .values("uid")
-    .toList();
-
   const peopleYouMayKnowPromise = g
     .V()
     .union(
@@ -256,40 +175,30 @@ async function GetRecommendationsOnboarding(
 
   const peopleInContactsPromise = g
     .V()
-    .union(
-      __.V()
-        .hasLabel("User")
-        .has("phoneNumber", within(contactsList))
-        .range(
-          offset_PeopleInContacts,
-          page_peopleInContacts * pagesize_peopleInContacts
-        ),
-      __.V()
-        .hasLabel("User")
-        .has("phoneNumber", within(FavcontactsList))
-        .range(
-          offset_PeopleInContacts,
-          page_peopleInContacts * pagesize_peopleInContacts
-        )
+    .hasLabel("User")
+    .has("username", username)
+    .outE("HAS_CONTACT")
+    .choose(
+      __.has("fav", true),
+      __.inV().property("weight", EmojiContactsWeightQuestions),
+      __.inV().property("weight", ContactsWeightQuestions)
+    )
+    .choose(
+      __.has("photo", true),
+      __.inV().property("weight", PhotoContactsWeightQuestions),
+      __.inV().property("weight", ContactsWeightQuestions)
     )
     .toList();
 
   const invitedPromise = IsUserInvited(username);
 
-  const [userFriends, PeopleYouMayKnow, PeopleInContacts, invited] =
+  const [ PeopleYouMayKnow, PeopleInContacts, invited] =
     await Promise.allSettled([
-      userFriendsPromise,
       peopleYouMayKnowPromise,
       peopleInContactsPromise,
       invitedPromise,
     ]);
 
-  const FilteredPeopleYouMayKnow = PeopleYouMayKnow.value.filter(
-    (user) => !userFriends.value.includes(user.username)
-  );
-  const FilteredPeopleInContacts = PeopleInContacts.value.filter(
-    (user) => !userFriends.value.includes(user.username)
-  );
 
   if (invited.value) PeopleYouMayKnow.value.concat(invited.value[0].inviter);
 
@@ -297,9 +206,9 @@ async function GetRecommendationsOnboarding(
   return {
     success: true,
     page_peopleYouMayKnow: page_peopleYouMayKnow,
-    People_You_May_Know: FilteredPeopleYouMayKnow,
+    People_You_May_Know: PeopleYouMayKnow,
     page_peopleInContacts: page_peopleInContacts,
-    PeopleInContacts: FilteredPeopleInContacts,
+    PeopleInContacts: PeopleInContacts,
     invited: invited.value,
   };
 }
@@ -352,20 +261,28 @@ async function GetRecommendationsExploreSection(
           offset_FriendsOfFriends,
           page_FriendsOfFriends * pagesize_FriendsOfFriends
         )
+
+        .not(__.inE("FRIENDS_WITH").has("uid", uid)) // Exclude users who are already friends
     )
     .toList();
 
   const AllUsersInContactsPromise = g
     .V()
     .hasLabel("User")
-    .union(
-      __.outE("contact")
-        .inV()
-        .range(offset_Contacts, page_Contacts * pagesize_Contacts),
-      __.outE("favcontact")
-        .inV()
-        .range(offset_Contacts, page_Contacts * pagesize_Contacts)
+    .has("username", username)
+    .outE("HAS_CONTACT")
+    .choose(
+      __.has("fav", true),
+      __.inV().property("weight", EmojiContactsWeightQuestions),
+      __.inV().property("weight", ContactsWeightQuestions)
     )
+    .choose(
+      __.has("photo", true),
+      __.inV().property("weight", PhotoContactsWeightQuestions),
+      __.inV().property("weight", ContactsWeightQuestions)
+    )
+    .not(__.inE("FRIENDS_WITH").has("uid", uid)) // Exclude users who are already friends
+    .range(offset_Contacts, page_Contacts * pagesize_Contacts)
     .toList();
 
   const FriendsOfFriendsPromise = g
@@ -376,23 +293,24 @@ async function GetRecommendationsExploreSection(
     .out("FRIENDS_WITH")
     .dedup()
     .where(P.neq("self"))
+    .not(__.inE("FRIENDS_WITH").has("uid", uid)) // Exclude users who are already friends
     .range(offset_SchoolUsers, page_SchoolUsers * pagesize_SchoolUsers)
     .valueMap("uid")
     .toList();
 
-    //#endregion
-
+  //#endregion
 
   const FriendRequestQuery = "SELECT friendRequests FROM friends WHERE uid =?";
   const friendRequestsPromise = client.execute(FriendRequestQuery, [uid], {
     prepare: true,
   });
 
-  const UserDataScyllaQuery = "SELECT contactsList,favcontactsList,FriendsCount FROM users WHERE uid = ?";
-  const UserDataScyllaPromise = client.execute(UserDataScyllaQuery,[uid]);
+  const UserDataScyllaQuery =
+    "SELECT contactsList,favcontactsList,FriendsCount FROM users WHERE uid = ?";
+  const UserDataScyllaPromise = client.execute(UserDataScyllaQuery, [uid]);
 
   const InviteSentQuery = "SELECT * FROM active_links WHERE inviter =?";
-  const AllInvitesSentPromise = client.execute(InviteSentQuery,[uid]);
+  const AllInvitesSentPromise = client.execute(InviteSentQuery, [uid]);
 
   const [
     userFriends,
@@ -401,7 +319,7 @@ async function GetRecommendationsExploreSection(
     friendRequests,
     AllUsersInContacts,
     AllUserData,
-    AllInvitesSent
+    AllInvitesSent,
   ] = await Promise.allSettled([
     userFriendsPromise,
     AllUsersInSchoolPromise,
@@ -409,62 +327,39 @@ async function GetRecommendationsExploreSection(
     friendRequestsPromise,
     AllUsersInContactsPromise,
     UserDataScyllaPromise,
-    AllInvitesSentPromise
+    AllInvitesSentPromise,
   ]);
 
   //#region Filtering
   // Filter out the existing friends from allUsers
-  const FilteredContactList = AllUserData[0].filter(
-    (user) => !userFriends.value.includes(user.uid)
-  );
-  const FilteredFavContactList = AllUserData[1].filter(
-    (user) => !userFriends.value.includes(user.uid)
-  );
-
-  const FilteredUsersInSchool = AllUsersInSchool.value.filter(
-    (user) => !userFriends.value.includes(user.uid)
-  );
-  const FilteredFriendsofFriends = FriendsOfFriends.value.filter(
-    (user) => !userFriends.value.includes(user.uid)
-  );
 
   const FilteredInvitationRecommendations = [
     ...new Set([...FilteredContactList, ...FilteredFavContactList]),
   ];
 
   await InsertMutualCount(uid, FilteredUsersInSchool);
-  await InsertMutualCount(uid, FilteredFriendsofFriends);
+  await InsertMutualCount(uid, FriendsOfFriends);
 
   //#endregion
 
   return {
     page_FriendsOfFriends: page_FriendsOfFriends,
-    FriendsOfFriends: FilteredFriendsofFriends,
+    FriendsOfFriends: FriendsOfFriends,
     UsersInContacts: AllUsersInContacts,
     page_SchoolUsers: page_SchoolUsers,
     UsersInSchool: FilteredUsersInSchool,
     InvitationRecommendation: FilteredInvitationRecommendations,
     TotalFriends: AllUserData[2],
     FriendRequests: friendRequests.value,
-    Invites:AllInvitesSent
+    InvitesSent: AllInvitesSent,
   };
 }
 
-async function GetRecommendationsQuestions(
-  username,
-  pagesize,
-  highschool,
-  grade
-) {
-  // Check user validity first
-  if (!(await CheckPlayerValidity(username))) {
-    return { success: false, error: "User does not exist" };
-  }
-
+async function GetRecommendationsQuestions(uid, highschool, grade) {
   const allUsers = await g
     .V()
     .hasLabel("User")
-    .has("username", username)
+    .has("username", uid)
     .union(
       // Contacts and FavContacts merged
       __.outE("HAS_CONTACT")
@@ -500,10 +395,11 @@ async function GetRecommendationsQuestions(
         .has("grade", grade)
         .property("weight", SameGradeWeightQuestions),
 
-      // Poll count
-      __.order()
+      // Users with property "PollsCount" and edge connection with our current user as "FRIENDS_WITH" (in descending order of PollsCount)
+      __.out("FRIENDS_WITH")
+        .order()
         .by("PollsCount", decr)
-        .property("weight", TopFriendsWeightsQuestions)
+        .property("weight", TopFriendsWeightsQuestions),
     )
     .local(
       __.repeat(__.sample(1).math("sin(random()) + _").is(P.gt(0))).times(4) // Return 4 users every time
@@ -524,7 +420,6 @@ async function GetRecommendationsQuestions(
 //#endregion
 
 module.exports = {
-  WeightArraysUsingProbability,
   FetchFriendsWithSubsActive,
   GetRecommendationsOnboarding,
   GetRecommendationsExploreSection,
