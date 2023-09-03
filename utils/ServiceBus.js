a4b1bd92948750b45c6efc7cf7bdc8f12ac31475
Daxx <64298070+Daxxboii@users.noreply.github.com>
2023-09-03 16:25:53 +0530

    Update ServiceBus.js

--- utils/ServiceBus.js
+++ utils/ServiceBus.js
@@ -1,29 +1,27 @@
-const  FetchFromSecrets  = require("./AwsSecrets.js").FetchFromSecrets;
-const cassandra = require('cassandra-driver');
+const FetchFromSecrets = require("./AwsSecrets.js").FetchFromSecrets;
+const cassandra = require("cassandra-driver");
 
 var queueURL;
 
 const UserCreation = require("./UserCreation.js");
 
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 const sqs = new AWS.SQS();
-const {SetupCassandraClient} = require('./SetupCassandra.js')
-
+const { SetupCassandraClient } = require("./SetupCassandra.js");
 
 var client;
 SetupCassandraClient(client).then(
   (Cassandraclient) => (client = Cassandraclient)
 );
 
-
-
 async function CheckRetry(uid, transactionId) {
   // Define retry limit
   const retryLimit = await KV.getKV("RetryTimes");
 
   // Fetch the transaction
-  const selectQuery = "SELECT retry_count FROM transactions WHERE transaction_id = ? AND uid = ?";
+  const selectQuery =
+    "SELECT retry_count FROM transactions WHERE transaction_id = ? AND uid = ?";
   const params = [transactionId, uid];
   const result = await client.execute(selectQuery, params, { prepare: true });
 
@@ -31,11 +29,16 @@ async function CheckRetry(uid, transactionId) {
     const retryCount = result.first().retry_count;
     if (retryCount < retryLimit) {
       // Increment retry count
-      const incrementRetryCountQuery = "UPDATE transactions SET retry_count = retry_count + 1 WHERE transaction_id = ? AND uid = ?";
-      await client.execute(incrementRetryCountQuery, [transactionId, uid], { prepare: true });
+      const incrementRetryCountQuery =
+        "UPDATE transactions SET retry_count = retry_count + 1 WHERE transaction_id = ? AND uid = ?";
+      await client.execute(incrementRetryCountQuery, [transactionId, uid], {
+        prepare: true,
+      });
       return true;
     } else {
-      console.log(`Transaction ${transactionId} for uid ${uid} has reached the retry limit.`);
+      console.log(
+        `Transaction ${transactionId} for uid ${uid} has reached the retry limit.`
+      );
       return false;
     }
   } else {
@@ -61,9 +64,9 @@ async function handleTransactionError(service, data) {
   }
 }
 
-
 //#region Fetching params stored in SQS , completely different from error handling
-async function fetchRequestsFromSQS(queueURL) { //TODO: fetch max requests from KV
+async function fetchRequestsFromSQS(queueURL) {
+  //TODO: fetch max requests from KV
   const params = {
     QueueUrl: queueURL,
     MaxNumberOfMessages: 10,
@@ -75,15 +78,18 @@ async function fetchRequestsFromSQS(queueURL) { //TODO: fetch max requests from 
       if (err) {
         reject(err);
       } else {
-        const deleteParams = {
-          QueueUrl: queueURL,
-          ReceiptHandle: data.Messages[0].ReceiptHandle
-        };
-        sqs.deleteMessage(deleteParams, function(err, data) {
-          if (err) {
-            console.warn("Delete Error", err);
-          } 
-        });
+        if (data.Messages.length > 0) {
+          const deleteParams = {
+            QueueUrl: queueURL,
+            ReceiptHandle: data.Messages[0].ReceiptHandle,
+          };
+
+          sqs.deleteMessage(deleteParams, function (err, data) {
+            if (err) {
+              console.warn("Delete Error", err);
+            }
+          });
+        }
         resolve(data.Messages || []);
       }
     });
@@ -94,4 +100,4 @@ async function fetchRequestsFromSQS(queueURL) { //TODO: fetch max requests from 
 
 //fetchRequestsFromSQS("https://sqs.us-east-1.amazonaws.com/422862242595/UserCreationSQSQueue");
 
-module.exports = {handleTransactionError,fetchRequestsFromSQS};
+module.exports = { handleTransactionError, fetchRequestsFromSQS };
