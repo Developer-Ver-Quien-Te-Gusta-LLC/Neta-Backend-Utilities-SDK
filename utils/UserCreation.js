44e535d79e6c64f7f8c6195dd2ca2874c8391688
niko89000 <nikoloco890@gmail.com>
2023-09-03 02:41:06 -0700

    removed spk

--- utils/UserCreation.js
+++ utils/UserCreation.js
@@ -177,7 +177,7 @@ async function createNeptuneUser(req) {
   try {
     await g
       .submit(
-        "g.addV('User').property('syntheticPartitionKey', 'anal').property('username', username).property('phoneNumber', phoneNumber).property('highschool', highschool).property('grade', grade).property('age', age).property('gender', gender).property('fname', fname).property('lname', lname).property('uid',uid)",
+        "g.addV('User').property('username', username).property('phoneNumber', phoneNumber).property('highschool', highschool).property('grade', grade).property('age', age).property('gender', gender).property('fname', fname).property('lname', lname).property('uid',uid)",
         {
           username: username,
           phoneNumber: phoneNumber,
