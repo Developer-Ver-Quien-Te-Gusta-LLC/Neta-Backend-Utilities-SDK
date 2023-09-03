5ba9d16bd534ea5c68edce930e1679a54088efe4
niko89000 <nikoloco890@gmail.com>
2023-09-03 04:03:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ module.exports = {
   SetupGraphDB: require('./utils/SetupGraphDB.js').SetupGraphDB,
   SetKV: KV.SetKV,
   getKV: KV.getKV,
-  SetupGeospatialDB: require('./utils/SetupGeospatialDB.js').SetupGeospatialDB,
+  SetupGeospatialDB: require('./utils/SetupGeospatialDB.js'),
   measureRouteComputeLength : Datadog.measureRouteComputeLength,
   logSanitizedRequest : Datadog.logSanitizedRequest,
   SendNotification: NotificationSystem.SendNotification,
