c29e3ab839c4f54277fea3d8ce371d19a09ec484
Daxx <64298070+Daxxboii@users.noreply.github.com>
2023-09-04 13:13:24 +0530

    Merge branch 'main' of https://github.com/Developer-Ver-Quien-Te-Gusta-LLC/Neta-Backend-Utilities-SDK

--- utils/Datadog.js
+++ utils/Datadog.js
@@ -1,44 +1,44 @@
 const AWSXRay = require('aws-xray-sdk-core');
+const expressMiddleware = AWSXRay.express;
 
 // Initialize AWS X-Ray
 AWSXRay.config([AWSXRay.plugins.EC2Plugin, AWSXRay.plugins.ECSPlugin]);
 AWSXRay.setContextMissingStrategy('LOG_ERROR');
+
 /**
  * Middleware to measure compute length of routes using AWS X-Ray.
  */
 const measureRouteComputeLength = (req, res, next) => {
-  const name = AWSXRay.middleware.resolveName(req.headers.host || 'unknown-host');
-  const segment = new AWSXRay.Segment('http.request', null, name);
-  AWSXRay.setSegment(segment);
-
-  res.on('finish', () => {
-    segment.addMetadata('http_status_code', res.statusCode);
-    segment.close();
-  });
-
+  const segment = AWSXRay.getSegment();
+  if (segment) {
+      res.on('finish', () => {
+          segment.addMetadata('http_status_code', res.statusCode);
+      });
+  }
   next();
 };
 
-/**
- * Middleware to capture and log the req object in an AWS X-Ray friendly way.
- */
 const logSanitizedRequest = (req, res, next) => {
   const sanitizedReq = {
-    method: req.method,
-    url: req.url,
-    headers: req.headers,
-    // Add other properties you want to log
+      method: req.method,
+      url: req.url,
+      headers: req.headers,
+      // Add other properties you want to log
   };
 
   // Capture the sanitized request in X-Ray
-  const subsegment = AWSXRay.getSegment().addNewSubsegment('logSanitizedRequest');
-  subsegment.addAnnotation('SanitizedRequest', JSON.stringify(sanitizedReq));
-  subsegment.close();
+  const segment = AWSXRay.getSegment();
+  if (segment) {
+      const subsegment = segment.addNewSubsegment('logSanitizedRequest');
+      subsegment.addAnnotation('SanitizedRequest', JSON.stringify(sanitizedReq));
+      subsegment.close();
+  }
 
   console.log('Sanitized Request:', sanitizedReq);
 
   next();
 };
+
 
 module.exports = {
   measureRouteComputeLength,
