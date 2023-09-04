617e19d55a1f806f204113e92f1f6f80d3d7ab39
graham-hologress <135217704+Ver-Quien-Te-Gusta-LLC@users.noreply.github.com>
2023-09-03 18:49:28 -0700

    Update SetupGeospatialDB.js

--- utils/SetupGeospatialDB.js
+++ utils/SetupGeospatialDB.js
@@ -2,6 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 const FetchFromSecrets = require('./AwsSecrets.js').FetchFromSecrets
 let _db;
 
+/*
 async function SetupGeospatialDB() {
     const cosmosDbEndpoint = await FetchFromSecrets("CosmosDBSpatialEndpoint");
     const cosmosDbKey = await FetchFromSecrets("CosmosDBSpatialKey");
@@ -22,6 +23,27 @@ async function SetupGeospatialDB() {
     await collection.createIndex({ location: "2dsphere" });
 
     return collection;
+}*/
+
+async function SetupGeospatialDB() {
+  const connectionString = "mongodb://netaschools:568eSJWdummLG5BY5NtDJC1rUFlQ1prabo8VFuvm8yG9RuKBFGr4QEhRyRv7kKMhK9lScIFrASdcACDb7Skqog==@netaschools.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@netaschools@";
+    const dbName = "schools";  // change this to your database name if different
+    let client;
+    try {
+        client = await MongoClient.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Successfully connected to the database!");
+
+        const db = client.db(dbName);
+        const collections = await db.listCollections().toArray();
+        //console.log("Collections in the database:", collections.map(coll => coll.name));
+        return collections
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+    } finally {
+        if (client) {
+            await client.close();
+        }
+    }
 }
 
 module.exports = { SetupGeospatialDB }
