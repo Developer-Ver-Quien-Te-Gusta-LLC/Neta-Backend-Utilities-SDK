2489a91520a957c261d7eca667e52160983e3b24
Daxx <64298070+Daxxboii@users.noreply.github.com>
2023-11-06 13:05:11 +0530

    Update AwsSecrets.js

--- utils/AwsSecrets.js
+++ utils/AwsSecrets.js
@@ -4,6 +4,7 @@ const projectID = "massive-boulder-403908";
 
 //fetch the accessid with the given key using AWS Secrets Manager
 async function FetchFromSecrets(key) {
+  try{
   const client = new SecretManagerServiceClient();
   const [version] = await client.accessSecretVersion({
     name: `projects/${projectID}/secrets/${key}/versions/latest`,
@@ -12,6 +13,11 @@ async function FetchFromSecrets(key) {
   const payload = version.payload.data.toString('utf8');
   //console.log(payload);
   return payload;
+}
+catch (error) {
+  console.error(`Failed to fetch secret ${secretName}:`, error);
+ // throw error;
+}
 }
 
 
