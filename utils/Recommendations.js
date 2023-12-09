866b8de23d5f8cab49905133c6f9ea9ed0f53733
Daxx <64298070+Daxxboii@users.noreply.github.com>
2023-12-09 05:30:48 +0530

    Test Global Search

--- utils/Recommendations.js
+++ utils/Recommendations.js
@@ -268,13 +268,13 @@ async function GetRecommendationsExploreSection(
 MATCH (user:User {uid: $uid})
 
 // Split the query into first name and last name
-WITH user, split($query, ' ') AS nameParts
+WITH user, CASE WHEN $query IS NOT NULL THEN split($query, ' ') ELSE [] END AS nameParts
 
 // 1. People in the same high school
 OPTIONAL MATCH (user)-[:ATTENDS_SCHOOL]->(school)
 WHERE school.name = $highschool
 OPTIONAL MATCH (otherUser:User)-[:ATTENDS_SCHOOL]->(school)
-WHERE user <> otherUser AND toLower(otherUser.fname) CONTAINS toLower(nameParts[0]) AND (size(nameParts) = 1 OR toLower(otherUser.lname) CONTAINS toLower(nameParts[1]))
+WHERE user <> otherUser AND size(nameParts) > 0 AND toLower(otherUser.fname) CONTAINS toLower(nameParts[0]) AND (size(nameParts) = 1 OR toLower(otherUser.lname) CONTAINS toLower(nameParts[1]))
 WITH user, nameParts, COLLECT(otherUser)[$offset_SchoolUsers..$limit_SchoolUsers] AS PeopleInSameSchool
 
 // 2. People in contacts
@@ -283,17 +283,17 @@ WITH user, nameParts, PeopleInSameSchool, COLLECT(contact)[$offset_Contacts..$li
 
 // 3. Friends of user's friends
 OPTIONAL MATCH (user)-[:FRIENDS_WITH]->(:User)-[:FRIENDS_WITH]->(friendsOfFriends:User)
-WHERE NOT (user)-[:FRIENDS_WITH]->(friendsOfFriends) AND user <> friendsOfFriends AND toLower(friendsOfFriends.fname) CONTAINS toLower(nameParts[0]) AND (size(nameParts) = 1 OR toLower(friendsOfFriends.lname) CONTAINS toLower(nameParts[1]))
+WHERE NOT (user)-[:FRIENDS_WITH]->(friendsOfFriends) AND user <> friendsOfFriends AND size(nameParts) > 0 AND toLower(friendsOfFriends.fname) CONTAINS toLower(nameParts[0]) AND (size(nameParts) = 1 OR toLower(friendsOfFriends.lname) CONTAINS toLower(nameParts[1]))
 WITH user, nameParts, PeopleInSameSchool, contacts, COLLECT(DISTINCT friendsOfFriends)[$offset_FriendsOfFriends..$limit_FriendsOfFriends] AS FriendsOfFriends
 
 // 4. People connected to user with HAS_CONTACT_IN_APP
 OPTIONAL MATCH (user)-[:HAS_CONTACT_IN_APP]->(hasContactInAppUser:User)
-WHERE toLower(hasContactInAppUser.fname) CONTAINS toLower(nameParts[0]) AND (size(nameParts) = 1 OR toLower(hasContactInAppUser.lname) CONTAINS toLower(nameParts[1]))
+WHERE size(nameParts) > 0 AND toLower(hasContactInAppUser.fname) CONTAINS toLower(nameParts[0]) AND (size(nameParts) = 1 OR toLower(hasContactInAppUser.lname) CONTAINS toLower(nameParts[1]))
 WITH user, PeopleInSameSchool, contacts, FriendsOfFriends, COLLECT(DISTINCT hasContactInAppUser)[$offset_Contacts..$limit_Contacts] AS ContactsInApp
 
 // 5. Other users with same fname or lname as query
 OPTIONAL MATCH (otherUser:User)
-WHERE toLower(otherUser.fname) CONTAINS toLower(nameParts[0]) AND (size(nameParts) = 1 OR toLower(otherUser.lname) CONTAINS toLower(nameParts[1]))
+WHERE size(nameParts) > 0 AND toLower(otherUser.fname) CONTAINS toLower(nameParts[0]) AND (size(nameParts) = 1 OR toLower(otherUser.lname) CONTAINS toLower(nameParts[1]))
 WITH user, PeopleInSameSchool, contacts, FriendsOfFriends, ContactsInApp, COLLECT(DISTINCT otherUser) AS OtherUsers
 
 RETURN {
