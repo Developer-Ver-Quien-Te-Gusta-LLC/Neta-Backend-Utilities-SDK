664d9d7c7d50b5cefc6c8c48411e3a5448c453e6
Daxx <64298070+Daxxboii@users.noreply.github.com>
2023-12-13 01:57:54 +0530

    Notification Intent

--- utils/NotificationSystem.js
+++ utils/NotificationSystem.js
@@ -1,6 +1,6 @@
-const  FetchFromSecrets  = require("./AwsSecrets.js").FetchFromSecrets;
-const  FetchChannelId  = require("./AlbyToken.js").FetchChannelId;
-const  getDataFromScyalla  = require("./DataBaseQueriesHandler.js").getDataFromScyalla;
+const FetchFromSecrets = require("./AwsSecrets.js").FetchFromSecrets;
+const FetchChannelId = require("./AlbyToken.js").FetchChannelId;
+const getDataFromScyalla = require("./DataBaseQueriesHandler.js").getDataFromScyalla;
 const getKV = require('./KV.js').getKV
 
 const Ably = require('ably');
@@ -10,7 +10,7 @@ var ably;
 let PubSubTopic;
 async function fetchAlby() {
   const key = await getKV("AblyAPIKey");
-  ably = new Ably.Realtime({key:key});
+  ably = new Ably.Realtime({ key: key });
   await ably.connection.once("connected");
   //sendRedundantly = getKV("RedundantNotifications")
 }
@@ -26,11 +26,11 @@ async function SetupFCM() {
   //import serviceAccount from './credentials/creds.json';
   Credentials = await FetchFromSecrets("FCMAccountCredentials");
   Credentials = JSON.parse(Credentials);
-  if(admin.apps.length ===0){
-  admin.initializeApp({
-    credential: admin.credential.cert(Credentials),
-  });
-}
+  if (admin.apps.length === 0) {
+    admin.initializeApp({
+      credential: admin.credential.cert(Credentials),
+    });
+  }
 }
 SetupFCM();
 
@@ -39,26 +39,26 @@ async function publishAlbyMessageNaive(user_id, message) {
 
   const channel = ably.channels.get(topicName);
 
-  if (typeof(message) == "object") message = JSON.stringify(message)
-  await channel.publish("event",message);
+  if (typeof (message) == "object") message = JSON.stringify(message)
+  await channel.publish("event", message);
 
 
   return { message: `Published a message to the topic: ${topicName}` };
 }
 
-async function publishAlbyMessage(ChannelID, message) {
+async function publishAlbyMessage(ChannelID, message,intent) {
   if (!ChannelID) {
     console.log("No topic name found for user");
     return { message: "No topic name found for user" };
   }
 
   const channel = ably.channels.get(ChannelID);
-  await channel.publish("event",message);
+  await channel.publish("event", message);
   console.log(`Published a message to the topic: ${ChannelID}`);
   return { message: `Published a message to the topic: ${ChannelID}` };
 }
 
-async function publishFCMMessage(userToken, message) {
+async function publishFCMMessage(userToken, message,intent) {
   if (!userToken) {
     console.error("User token is not provided");
     return;
@@ -68,8 +68,7 @@ async function publishFCMMessage(userToken, message) {
     token: userToken,
     notification: {
       title: "Neta",
-      //body: typeof(message) == "object" ? message : JSON.stringify(message),
-      body: NotifTitle.replace("{GENDER}",message.askedgender).replace("{SCHOOL}",message.askedschool)
+      body: intent == "poll" ? message.friend + "Send you a Friend Request" : NotifTitle.replace("{GENDER}", message.askedgender).replace("{SCHOOL}", message.askedschool)
     },
   };
 
@@ -77,40 +76,32 @@ async function publishFCMMessage(userToken, message) {
     .messaging()
     .send(payload)
     .then((response) => {
-      console.log("Notification sent successfully:", response);
+      //console.log("Notification sent successfully:", response);
     })
     .catch((error) => {
-      console.error("Error sending notification:", error);
+      console.error(`Error sending notification: ${error}`);
     });
 }
 
-async function SendNotification(uid, payload) {
-  /*if (sendRedundantly) {
+async function SendNotification(uid, payload, intent) {
+  try {
+    const ChannelID = await FetchChannelId(uid);
+    await publishAlbyMessage(ChannelID, payload,intent);
     const userToken = await getDataFromScyalla("users", uid, "FCMToken");
-    await Promise.allSettled([publishAlbyMessage(uid, payload), publishFCMMessage(userToken, JSON.stringify(payload))])
-  }*/
-  try{
-  const ChannelID = await FetchChannelId(uid);
-
-  await publishAlbyMessage(ChannelID, payload);
-
-  const userToken = await getDataFromScyalla("users", uid, "FCMToken");
-
-  await publishFCMMessage(userToken, payload);
-
-  console.log("RT Notificaiton Sent");
+    await publishFCMMessage(userToken, payload,intent);
+    console.log("RT Notificaiton Sent");
   }
-  catch(err){
-    console.log("Error Sending RT notif",err);
+  catch (err) {
+    console.error(`Error Sending RT notif: ${err}`);
   }
 }
 
 
-const {PubSub} = require('@google-cloud/pubsub');
+const { PubSub } = require('@google-cloud/pubsub');
 const pubSubClient = new PubSub();
 
 
-async function PublishDelayedNotif(data,Timeout){ //make sure data is string and Timeout is minutes
+async function PublishDelayedNotif(data, Timeout) { //make sure data is string and Timeout is minutes
   try {
     const dataBuffer = Buffer.from(data);
     const messageId = await pubSubClient
@@ -124,4 +115,4 @@ async function PublishDelayedNotif(data,Timeout){ //make sure data is string and
     console.error(`Error publishing message: ${error}`);
   }
 }
-module.exports = { SendNotification,publishFCMMessage ,publishAlbyMessage, publishAlbyMessageNaive,PublishDelayedNotif };
+module.exports = { SendNotification, publishFCMMessage, publishAlbyMessage, publishAlbyMessageNaive, PublishDelayedNotif };
