84b42547e414a0cdd2ec0e56ac6b171013c74b94
Daxx <64298070+Daxxboii@users.noreply.github.com>
2024-01-19 22:13:01 +0530

    Update PhoneNumberFormatter.js

--- utils/PhoneNumberFormatter.js
+++ utils/PhoneNumberFormatter.js
@@ -2,13 +2,13 @@ function formatPhoneNumber(phoneNumber) {
     // Remove all non-digit characters
     let cleanedNumber = phoneNumber.replace(/\D/g, '');
 
-    // If cleanedNumber length is 12, remove the first two digits
+    /*// If cleanedNumber length is 12, remove the first two digits
     if (cleanedNumber.length === 12) {
         cleanedNumber = cleanedNumber.substring(2);
     }
     else if(cleanedNumber.length === 11){
         cleanedNumber = cleanedNumber.substring(1);
-    }
+    }*/
 
     return cleanedNumber;
 }
